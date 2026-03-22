c1f97b2783667d11c9abef2ae00b826e30c69070
Chr0mium24 <3294212389@qq.com>
2026-03-23 02:08:44 +0800

    fix(editor): load crepe common theme css

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
+import '@milkdown/crepe/theme/common/style.css';
 import '@milkdown/crepe/theme/nord.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
